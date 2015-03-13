57884729c9a1437f6fb236572c44b08ee13d0ffe
ivxvi12 <ivxvi12@nate.com>
2015-03-13 19:41:16 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,1 @@
-var http= require('http'); 
-
-http.createServer(function(req,res){
-	res.writeHead(200,{"Content-Type":"text/plain"});
-	res.write('helloewordl');
-	res.end();
-	}).listen(8001);
- 
 console.log('server has started.');
