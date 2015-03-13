e4afaf47200912c1a21690526a4dbd0a43303720
ivxvi12 <ivxvi12@nate.com>
2015-03-13 18:45:10 +0900

    dd

--- web.js
+++ web.js
@@ -2,7 +2,8 @@ var http= require('http');
 
 http.createServer(function(req,res){
 	res.writeHead(200,{"Content-Type":"text/plain"});
-	res.wirte('helloewordl');
-	res.end();}).listen(8001);
+	res.write('helloewordl');
+	res.end();
+	}).listen(8001);
  
 console.log('server has started.');
